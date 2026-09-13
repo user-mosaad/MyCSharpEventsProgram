d0b40fe3bb1cc69f18771867a6f7e40ceac7c800
user-mosaad <msaadshahid999@gmail.com>
2026-09-13 15:30:48 +0600

    Write the code for the events

--- MyCSharpEventsProgram/Program.cs
+++ MyCSharpEventsProgram/Program.cs
@@ -1,1 +1,68 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public delegate void Update();
+
+
+/// <summary>
+/// Publisher here.
+/// </summary>
+class UpdateStuff
+{
+    public event Update UpdateItemEvent;
+    public event Update UpdateSaleEvent;
+    public event Update UpdatePurchaseEvent;
+
+    public void UpdateItem()
+    {
+        Console.WriteLine("Updated item.");
+        UpdateItemEvent();
+    }
+
+    public void UpdateSale()
+    {
+        Console.WriteLine("Updated sale.");
+        UpdateSaleEvent();
+    }
+
+    public void UpdatePurchase()
+    {
+        Console.WriteLine("Updated purchase.");
+        UpdatePurchaseEvent();
+    }
+}
+
+/// <summary>
+/// Subscribers to the event here.
+/// </summary>
+class GetNotifications
+{
+    public void ItemUpdateNotification()
+    {
+        Console.WriteLine("Notification: the item has been updated.");
+    }
+
+    public void SaleUpdateNotification()
+    {
+        Console.WriteLine("Notification: the sale has been updated.");
+    }
+
+    public void PurchaseUpdateNotification()
+    {
+        Console.WriteLine("Notification: the purchase has been updated.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        UpdateStuff updateStuff = new UpdateStuff();
+        GetNotifications getNotifications = new GetNotifications();
+
+        // Subscribe to the event
+        updateStuff.UpdateItemEvent += getNotifications.ItemUpdateNotification;
+        updateStuff.UpdateSaleEvent += getNotifications.SaleUpdateNotification;
+        updateStuff.UpdatePurchaseEvent += getNotifications.PurchaseUpdateNotification;
+
+        // Trigger the event, notifying all subscribers
+        updateStuff.UpdatePurchase();
+    }
+}
